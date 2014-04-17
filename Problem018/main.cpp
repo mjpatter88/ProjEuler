7a7c86719f9351c0e656cc98f410d4d15963357e
Michael Patterson <mjpatter88@gmail.com>
2014-04-17 16:57:58 -0500

    Added support for command line arguments and set up the structure to use multiple algorithms.

--- Problem018/main.cpp
+++ Problem018/main.cpp
@@ -4,20 +4,57 @@
 #include <iostream>
 #include <fstream>
 #include <algorithm>
+#include <string>
 
 #define NUM_ROWS 15
+#define DEBUG false
 
 void bruteSolve(int** arr);
 void dynSolve(int **arr);
+void bottomUpSolve(int **arr);
+
 void printArr(int **arr);
-int recSolve(int** arr, int row, int col);
+int recBrute(int** arr, int row, int col);
 
 int main(int argc, char* argv[])
 {
     /*
      * For now we will be using jagged 2D arrays for lack of a better data structure.
-     * I'll write a "slow" brute force method and a "fast" dynamic programming one.
+     * I'll write 3 different functions utilizing different methods.     
+     * 1) Slow brute force method "bruteSolve"
+     * 2) Faster dynamic programming method "dynSolve"
+     * 3) Clever bottom-up summing method "bottomUpSolve"
      */
+
+    //First handle command line parameters
+    void (*pSolve)(int**) = bottomUpSolve;
+    if(DEBUG)
+    {
+        std::cout << "Command line args: " << argc << std::endl;
+    }
+    if(argc > 1)
+    {
+        if(std::string("-h").compare(argv[1]) == 0)
+        {
+            std::cout << "Usage: ./main [flags]" << std::endl;
+            std::cout << "\t-b for brute force solution" << std::endl;
+            std::cout << "\t-d for dynamic programming solution" << std::endl;
+            std::cout << "\t-c for a clever bottom up solution (default)" << std::endl;
+            return 0;
+        }
+        else if(std::string("-b").compare(argv[1]) == 0)
+        {
+            pSolve = bruteSolve;
+        }
+        else if(std::string("-d").compare(argv[1]) == 0)
+        {
+            pSolve = dynSolve;
+        }
+        else if(std::string("-c").compare(argv[1]) == 0)
+        {
+            pSolve = bottomUpSolve;
+        }
+    }
 
     //First we need an array of pointers
     int** arr = new int*[NUM_ROWS];
@@ -26,7 +63,7 @@ int main(int argc, char* argv[])
         arr[i] = new int[i+1];  //Each row has one more element than the previous
     }
 
-
+    //Fill the arrays with the numbers from the file.
     std::string fileName = "numbers.txt";
     std::ifstream inputFile(fileName.c_str());
     if(!inputFile.is_open())
@@ -34,7 +71,6 @@ int main(int argc, char* argv[])
         std::cerr << "Unable to open file: " << fileName << std::endl;
         return 1;
     }
-
     //We basically assume that the file is formatted correctly for now
     int num;
     int currentRow = 0;
@@ -49,9 +85,16 @@ int main(int argc, char* argv[])
         }
         currentRow++;
     }
-    printArr(arr);
-    bruteSolve(arr);
+
+    if(DEBUG)
+    {
+        printArr(arr);
+    }
+
+    pSolve(arr);
+    //bruteSolve(arr);
     //dynSolve(arr);
+    //bottomUpSolve(arr);
 
 
     //Delete each inner array
@@ -66,12 +109,16 @@ int main(int argc, char* argv[])
 
 void bruteSolve(int** arr)
 {
-    int max = recSolve(arr, 0, 0);
+    if(DEBUG)
+    {
+        std::cout << "Brute force soltuion..." << std::endl;
+    }
+    int max = recBrute(arr, 0, 0);
     std::cout << "Answer: " << max << std::endl;
     return;
 }
 
-int recSolve(int** arr, int row, int col)
+int recBrute(int** arr, int row, int col)
 {
     // Break condition when it's in the last row of the pyramid
     if(row == NUM_ROWS-1)
@@ -81,13 +128,26 @@ int recSolve(int** arr, int row, int col)
     else
     {
         // This node's value plus max of left child node and right child node
-        int max = arr[row][col] + std::max(recSolve(arr, row+1, col), recSolve(arr, row+1, col+1));
+        int max = arr[row][col] + std::max(recBrute(arr, row+1, col), recBrute(arr, row+1, col+1));
         return max;
     }
 }
 
 void dynSolve(int **arr)
 {
+    if(DEBUG)
+    {
+        std::cout << "Dynamic programming soltuion..." << std::endl;
+    }
+    return;
+}
+
+void bottomUpSolve(int **arr)
+{
+    if(DEBUG)
+    {
+        std::cout << "Clever bottum-up soltuion..." << std::endl;
+    }
     return;
 }
 
