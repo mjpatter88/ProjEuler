6b0bd04ecd414d33068c6c3c224b64d33e22402e
Michael Patterson <mjpatter88@gmail.com>
2015-04-18 18:56:29 -0400

    Completed the solution for Problem 25.

--- Problem025/Problem_025_Solver.py
+++ Problem025/Problem_025_Solver.py
@@ -1,7 +1,28 @@
-# Project Euler - Problem 25
+#!/usr/bin/python3
+'''
+Project Euler Problem 25:
+
+What is the first term in the Fibonacci sequence to contain 1000 digits?
+
+Answer: 4782
+
+'''
+
+numbers = [1, 1, 2, 3, 5] # Seed with a few starting values
+
+def getNextFib():
+    length = len(numbers)
+    numbers.append(numbers[length-2] + numbers[length-1])
+    return numbers[length]
 
 def main():
-    print("Running Main.")
+    print("Solving Problem 25...")
+    number = 0
+    while len(str(number)) < 1000:
+        number = getNextFib()
+
+    print(number)
+    print("Term: " + str(len(numbers)))
 
 if __name__ == '__main__':
     main()
