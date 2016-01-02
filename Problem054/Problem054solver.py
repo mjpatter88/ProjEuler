6d7ea2181cb6c2dda634309eb8eccf14fb230311
Michael Patterson <mjpatter88@gmail.com>
2016-01-01 22:00:26 -0600

    Added method signatures for rank, value, and comparison functions. No implementation yet.

--- Problem054/Problem054solver.py
+++ Problem054/Problem054solver.py
@@ -1,8 +1,40 @@
 # Python 3
 
 class PokerHand():
+    """
+    """
     def __init__(self, hands):
+        """
+        """
         assert len(hands) == 5, "You must supply 5 cards. You supplied {0} cards.".format(len(hands))
+
+    def rank(self):
+        """
+        """
+        pass
+
+    def value(self):
+        """
+        """
+        pass
+
+# https://docs.python.org/3/reference/datamodel.html#object.__lt__
+
+    def __eq__(self, other):
+        """
+        """
+        pass
+
+    def __lt__(self, other):
+        """
+        """
+        pass
+
+    def __gt__(self, other):
+        """
+        """
+        pass
+
 
 if __name__ == '__main__':
     with open('poker.txt') as input_file:
