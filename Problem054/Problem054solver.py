8d48aecb09bf5d15974f4cd0e8d92cd4d8aea007
Michael Patterson <mjpatter88@gmail.com>
2016-01-01 21:48:44 -0600

    PokerHand class with basic constructor. Loop that splits each line into two poker hands.

--- Problem054/Problem054solver.py
+++ Problem054/Problem054solver.py
@@ -1,7 +1,14 @@
 # Python 3
 
+class PokerHand():
+    def __init__(self, hands):
+        assert len(hands) == 5, "You must supply 5 cards. You supplied {0} cards.".format(len(hands))
 
 if __name__ == '__main__':
     with open('poker.txt') as input_file:
         for line in input_file:
-            print(line)
+            player1_cards = line.split(" ")[:5]
+            player2_cards = line.split(" ")[5:]
+            player1_hand = PokerHand(player1_cards)
+            player2_hand = PokerHand(player2_cards)
+
