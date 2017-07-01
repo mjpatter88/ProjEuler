9f28e75a7848c5147615371ba45ec7569c7961ce
Michael Patterson <mjpatter88@gmail.com>
2017-07-01 15:26:00 -0500

    Problem 92 performance work.

--- Problem092/solver.py
+++ Problem092/solver.py
@@ -6,7 +6,8 @@ def sum_of_digit_squares(number):
 # Square digit number chains always arrive at either 1 or 89.
 # This dictionary is a mapping from start to either 1 or 89
 # It can be used as a cache, since chains at x always resolve consistently.
-results = {1:1, 89:89}
+resolve_to_1 = {1}
+resolve_to_89 = {89}
 nums = {x for x in range(1, 10000000)}
 
 while nums:
@@ -14,17 +15,19 @@ while nums:
     num = nums.pop()
     chain.append(num)
 
-    while num != 1 and num != 89:
-        if num in results:
-            num = results[num]
+    while True:
+        if num in resolve_to_1:
+            resolve_to_1.update(chain)
+            nums.difference_update(chain)
+            break
+        elif num in resolve_to_89:
+            resolve_to_89.update(chain)
+            nums.difference_update(chain)
+            break
         else:
             num = sum_of_digit_squares(num)
             chain.append(num)
 
-    for start in chain:
-        results[start] = num
-        nums.discard(start)
 
-
-count = sum(1 for res in results.values() if res == 89)
+count = len(resolve_to_89)
 print(f"Number of starting numbers below ten million that arrive at 89: {count}")
